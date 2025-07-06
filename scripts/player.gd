13b9d0b991c26a2d13e67e4d4be07e8f2d636317
Paul Fok <paul@anthonyfok.org>
2025-07-06 13:05:16 -0600

    Improved ship physics

--- scripts/player.gd
+++ scripts/player.gd
@@ -35,20 +35,31 @@ func ship(delta: float) -> void:
 	
 	if is_on_wall():
 		kill()
-	
+		
 	if is_on_floor() or is_on_ceiling():
-		velocity.y = 0
+		velocity.y /= 2 * delta
+		$Sprite.rotation_degrees = ($Sprite.rotation_degrees + round($Sprite.rotation_degrees / 90) * 90) / 18
 	
 	if Input.is_action_pressed("jump"):
-		velocity.y -= 500 * delta
+		$Sprite.rotation_degrees -= 135 * delta
 	else:
-		velocity.y += 500 * delta
+		$Sprite.rotation_degrees += 135 * delta
 		
-	if velocity.y > 1000:
-			velocity.y = 1000
+		
+	if $Sprite.rotation_degrees > 45:
+			$Sprite.rotation_degrees = 45
 	
-	if velocity.y < -1000:
-			velocity.y = -1000
+	if $Sprite.rotation_degrees < -45:
+			$Sprite.rotation_degrees = -45
+		
+	velocity = $Sprite.transform.x * 500
+	velocity.x = 0
+		
+	if velocity.y > 750:
+			velocity.y = 750
+	
+	if velocity.y < -750:
+			velocity.y = -750
 
 func kill(time = 0.75, start_delay = 0.25):
 	if alive:
