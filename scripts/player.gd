e442b5da77b6c2b3c0ecc7ac84aeae2a7892ea6b
Paul Fok <paul@anthonyfok.org>
2025-07-06 13:13:20 -0600

    Fix goofy ahh merge

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,26 +7,11 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
 		kill(0)
 	if alive:
-<<<<<<< HEAD
 		match gamemode:
 			"cube": cube(delta)
 			"ship": ship(delta)
 
-		position.x += 937 * delta
-=======
-		if velocity.y > 5000:
-			velocity.y = 5000
-		if is_on_wall():
-			kill()
 		position.x += 975 * delta
-		if not is_on_floor():
-			velocity += get_gravity() * delta
-			$Sprite.rotate(6.5 * delta)
-		else:
-			$Sprite.rotation_degrees = ($Sprite.rotation_degrees + round($Sprite.rotation_degrees / 90) * 90) / 2
-			
-		if Input.is_action_pressed("jump") and is_on_floor():
-			velocity.y = JUMP_VELOCITY
 		move_and_slide()
 	
 func cube(delta: float) -> void:
