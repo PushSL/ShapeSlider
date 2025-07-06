f2309d3f88ed16cdbcb60d2b229ad27486fb11bc
Paul Fok <paul@anthonyfok.org>
2025-07-05 22:19:35 -0600

    modified:   scripts/player.gd

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,12 @@
 extends CharacterBody2D
-
 const JUMP_VELOCITY = -1900
-var dead: bool = false
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
 		kill(0)
 	if velocity.y > 5000:
 		velocity.y = 5000
-	
-	if !dead:
-		velocity.x = 937
-	
+	position.x += 937 * delta
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		$Sprite.rotate(6.5 * delta)
@@ -23,14 +18,17 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func kill(time = 0.75):
-	dead = true
-	await get_tree().create_timer(time).timeout
-	$Sprite/Camera2D.position_smoothing_enabled = false
-	position = Vector2(48, -48)
-	velocity = Vector2.ZERO
-	$Sprite.rotation_degrees = 0
-	dead = false
-	$Sprite/Camera2D.position_smoothing_enabled = true
+	if get_tree().paused == false:
+		$Camera2D.position_smoothing_enabled = false
+		get_tree().paused = true
+		if time != 0:
+			await get_tree().create_timer(time).timeout
+		$Sprite.rotation_degrees = 0
+		position = Vector2(48, -48)
+		velocity = Vector2.ZERO
+		get_tree().paused = false
+		await get_tree().create_timer(0).timeout
+		$Camera2D.position_smoothing_enabled = true
 
 func _on_hitbox_area_entered(area: Area2D) -> void:
 	if area.name == "Spike":
