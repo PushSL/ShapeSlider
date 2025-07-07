6cfe1cc0e2fd7fedd4577f12dfa61b71dc22de21
Paul Fok <paul@anthonyfok.org>
2025-07-07 16:04:04 -0600

    eeae

--- scripts/interpolate.gd
+++ scripts/interpolate.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var m_Pos_prev : = Vector2(0,0)
+var m_Pos_prev: = Vector2(0,0)
 var m_Pos_curr: = Vector2(0,0)
 var parent: Node2D
 
@@ -11,7 +11,6 @@ func _ready():
 	set_global_position(parent.get_global_position())
 
 func _process(_delta):
-
 	if Engine.get_frames_per_second() > Engine.physics_ticks_per_second:
 		set_global_position(m_Pos_prev.lerp(m_Pos_curr, Engine.get_physics_interpolation_fraction()))
 
